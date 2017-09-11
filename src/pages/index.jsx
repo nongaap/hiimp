318db575216ce4ad9b80be85818a168cd66569ae
Mike Puangmalai <mike@nongaap.com>
2017-09-11 16:40:07 -0700

    Home index.jsx file updated.

---
+++ src/pages/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+import Fullscreen from '../components/fullscreen';
+import Meta from '../components/meta';
+import Wrapper from '../components/wrapper';
+
+import styles from './styles.module.css';
+
+const Index = ({ location }) => (
+  <div>
+    <Meta location={location} />
+    <Fullscreen className={styles.intro} firstItem>
+      <Wrapper>
+        <h1>
+          Hi. I’m {'{'}Mike{'}'} P.
+          <br />
+          <br />
+          I’m a software developer & tech analyst.
+          <br />
+          <br />
+          Check out some of my <Link className={styles.link} to="/projects">projects</Link>.
+        </h1>
+      </Wrapper>
+    </Fullscreen>
+  </div>
+);
+
+export default Index;
